4f284102c303028b75e493d436bffcdfbc39288c
Martin <108198542+mfvc29@users.noreply.github.com>
2024-09-20 19:47:02 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-onload = () => {
-  document.body.classList.remove("container");
-};
-
-document.addEventListener('DOMContentLoaded', function() {
-  const textoContainer = document.getElementById('texto-container');
-  textoContainer.innerHTML = "Aunque nos conocemos<br> desde hace tiempo,<br>hay tanto por descubrir y <br>algo nuevo que explorar";
-});
-
