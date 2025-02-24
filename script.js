c3b0ed1a18ec8fc5278efb516ad04c4db88c8ecd
Martin <108198542+mfvc29@users.noreply.github.com>
2025-02-24 09:59:34 -0500

    script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ onload = () => {
 
 document.addEventListener('DOMContentLoaded', function() {
   const textoContainer = document.getElementById('texto-container');
-  textoContainer.innerHTML = "Aunque nos conocemos desde hace pocos días,<br> ya siento que hay mucho por descubrir,<br> y un montón de cosas nuevas por explorar juntos.<br> Te amo ❤️❤️";
+  textoContainer.innerHTML = "Aunque nos conocemos desde hace pocos días,<br> ya siento que hay mucho por descubrir,<br> y un montón de cosas nuevas por explorar juntos.<br> Te amo Lily❤️❤️";
 });
 
